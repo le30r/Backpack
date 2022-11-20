02339574708d47dd928a99b48258e08e40b6aead
Ivan Marinin <marivann2001@gmail.com>
2022-11-20 22:00:46 +0300

    added BackpackApp

---
+++ BackpackApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Backpack;
+
+BranchAndBoundBackpack backpack = new BranchAndBoundBackpack(15);
+var items = TaskGenerator.GetPredictableTask().Item1;
+
+var result = backpack.SolveZeroOne(items);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(i + " " + items[i].Weight + " " + result[i]);
+}
